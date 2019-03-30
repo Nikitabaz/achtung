468842f53a9a719e1a1e1adfd2ab6bb5d42b44fb
SwatGetmann <swatgetmann@gmail.com>
2019-03-30 13:44:10 +0300

    fix

--- main.rb
+++ main.rb
@@ -88,21 +88,3 @@ end
 get '/index' do
   File.read(File.join('public', 'index.html'))
 end
-
-# get '/oauth2callback' do
-#   client_secrets = Google::APIClient::ClientSecrets.load
-#   auth_client = client_secrets.to_authorization
-#   auth_client.update!(
-#       :scope => 'https://www.googleapis.com/auth/drive.metadata.readonly',
-#       :redirect_uri => url('/oauth2callback'))
-#   if request['code'] == nil
-#     auth_uri = auth_client.authorization_uri.to_s
-#     redirect to(auth_uri)
-#   else
-#     auth_client.code = request['code']
-#     auth_client.fetch_access_token!
-#     auth_client.client_secret = nil
-#     session[:credentials] = auth_client.to_json
-#     redirect to('/')
-#   end
-# end
